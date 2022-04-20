20d192c5d0f70365f0cc34c0c25a0de9b22fc072
Irina <89231707647@mail.ru>
2022-04-20 12:26:45 +0700

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные- 
+// числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int M = Prompt("Введите число M => ");
+int N = Prompt("Введите число N => ");
+
+
+NaturalRow(M, N);
+
+void NaturalRow(int M, int N)
+{
+    if (M > N) return;
+    {
+        if (M % 2 == 0)
+            Console.WriteLine(M + " ");
+        NaturalRow(M + 1, N);
+
+    }
+
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int M = Prompt("Введите число M => ");
+int N = Prompt("Введите число N => ");
+
+
+NaturalRow(M, N);
+
+void NaturalRow(int M, int N, int sum = 0)
+{
+    if (M > N) return;
+    {
+        sum = sum + M;
+    NaturalRow(M + 1, N);
+    }
+    Console.WriteLine(sum + " ");
+}
+     
